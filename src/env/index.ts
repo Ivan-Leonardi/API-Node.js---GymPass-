bfb6863043995445d0a89abee5d4299f55a7ca8c
Ivan-Leonardi <ivanthaisjesus@gmail.com>
2026-09-08 21:28:23 -0300

    feat: add dotenv and zod config environment variables

---
+++ src/env/index.ts
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import { z } from "zod";
+
+
+//Validação das variáveis de ambiente
+const envSchema = z.object({
+  NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
+  PORT: z.coerce.number().default(3333),
+})
+ 
+const _env = envSchema.safeParse(process.env); //safeParse verifica se dentro do objeto process.env existe as variáveis de ambiente que foram definidas no envSchema
+
+if (_env.success === false) {
+  console.error("Invalid environment variables", _env.error.format());
+  throw new Error("Invalid environment variables.");
+}
+
+export const env = _env.data;
